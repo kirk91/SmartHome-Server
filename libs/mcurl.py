50cc0ab31531cfcfa6879d9f10d424ddfd4087b4
steven.hl <steven.hl.0901@gmail.com>
2015-03-01 17:13:04 +0800

    fix url bug

--- libs/mcurl.py
+++ libs/mcurl.py
@@ -26,8 +26,7 @@ class CurlHelper(object):
     def post(self,url,params=None,data=""):
         if params:
             url = url % params
-        print url,type(url)
-        self.curl.setopt(pycurl.URL, url.encode('utf-8'))
+        self.curl.setopt(pycurl.URL, url.encode('utf-8')) #注意字符编码问题
         self.curl.setopt(pycurl.POSTFIELDS, data)
         b = StringIO.StringIO()
         self.curl.setopt(pycurl.WRITEFUNCTION, b.write)
