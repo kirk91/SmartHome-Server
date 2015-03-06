b3efb6051e99509dfcea400e9143f6358989cd52
steven.hl <steven.hl.0901@gmail.com>
2015-03-06 14:38:52 +0800

    增加一些注释

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+# start socket_server
 python socket_server.py 1>>/var/log/raspberry-pi-wechat/socket_server.log 2>>/var/log/raspberry-pi-wechat/socket_server.log &
 
+# start wechat_server
 python wechat_server.py 1>>/var/log/raspberry-pi-wechat/wechat_server.log 2>>/var/log/raspberry-pi-wechat/wechat_server.log &
