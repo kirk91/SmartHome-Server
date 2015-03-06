8fab33cbc4fe06bab82158e2562ac13fa3667346
steven.hl <steven.hl.0901@gmail.com>
2015-03-06 11:35:17 +0800

    修改脚本运行时所需要的权限

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-sudo python socket_server.py 1>>/var/log/raspberry-pi-wechat/socket_server.log 2>>/var/log/raspberry-pi-wechat/socket_server.log &
-sudo python wechat_server.py 1>>/var/log/raspberry-pi-wechat/wechat_server.log 2>>/var/log/raspberry-pi-wechat/wechat_server.log &
+python socket_server.py 1>>/var/log/raspberry-pi-wechat/socket_server.log 2>>/var/log/raspberry-pi-wechat/socket_server.log &
+
+python wechat_server.py 1>>/var/log/raspberry-pi-wechat/wechat_server.log 2>>/var/log/raspberry-pi-wechat/wechat_server.log &
