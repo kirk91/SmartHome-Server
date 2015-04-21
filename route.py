f64c28c02a1d3187527f92cf97341a74e25bb0ff
steven-hl <steven.hl.0901@gmail.com>
2015-04-21 12:05:20 +0800

    fix bug

--- route.py
+++ route.py
@@ -7,7 +7,7 @@ urls = [
     (r'/', handler.index.IndexHandler),
     (r'/weixin', handler.weixin.WechatHandler),
     (r'/weixin/oauth', handler.oauth.OauthHandler),
-    (r'/wexin/auth', handler.oauth.AuthHandler),
+    (r'/weixin/auth', handler.oauth.AuthHandler),
     (r'/device/qr', handler.qr.QrcodeHandler),
     (r'/device/sensor/data', handler.device.DataHandler),
     (r'/device/new', handler.device.DeviceInfoHandler),
